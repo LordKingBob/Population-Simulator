92d0ec5d9baffe97676a3e17b9ab2ba6cbebc8e8
Ivan Shuvalov <lordkingbobminecraft@gmail.com>
2022-05-15 13:57:30 -0400

    Initial Project Setup

---
+++ src/main/java/com/sci/populationsimulator/App.java
@@ -0,0 +1,13 @@
+package com.sci.populationsimulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
